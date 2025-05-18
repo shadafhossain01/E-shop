17f8eb9609a432d4677c048550c6effdcaebd79a
Shadaf Hossain <shadafhossain01@gmail.com>
2025-05-18 17:02:19 +0600

    Facilities Done

--- src/components/Facilities.jsx
+++ src/components/Facilities.jsx
@@ -13,10 +13,10 @@ const Facilities = () => {
           <div className="flex items-center gap-6">
             <Headset />
             <div>
-              <h6 className="text-[#303030] font-bold font-[Montserrat] text-base">
+              <h6 className="text-[#303030] font-bold font-['Montserrat'] text-base leading-[24px]">
                 Responsive
               </h6>
-              <p className="text-[#303030] font-normal font-[Montserrat] text-base">
+              <p className="text-[#303030] font-normal font-['Montserrat'] text-base">
                 Customer service available 24/7
               </p>
             </div>
@@ -24,10 +24,10 @@ const Facilities = () => {
           <div className="flex items-center gap-6">
             <Security />
             <div>
-              <h6 className="text-[#303030] font-bold font-[Montserrat] text-base">
+              <h6 className="text-[#303030] font-bold font-['Montserrat'] text-base leading-[24px]">
                 Secure
               </h6>
-              <p className="text-[#303030] font-normal font-[Montserrat] text-base">
+              <p className="text-[#303030] font-normal font-['Montserrat'] text-base">
                 Certified marketplace since 2017
               </p>
             </div>
@@ -35,21 +35,21 @@ const Facilities = () => {
           <div className="flex items-center gap-6">
             <Delivery />
             <div>
-              <h6 className="text-[#303030] font-bold font-[Montserrat] text-base">
+              <h6 className="text-[#303030] font-bold font-['Montserrat'] text-base leading-[24px]">
                 Shipping
               </h6>
-              <p className="text-[#303030] font-normal font-[Montserrat] text-base">
+              <p className="text-[#303030] font-normal font-['Montserrat'] text-base">
                 Free, fast, and reliable worldwide
               </p>
             </div>
           </div>
           <div className="flex items-center gap-6">
-            <ReturnItem/>
+            <ReturnItem />
             <div>
-              <h6 className="text-[#303030] font-bold font-[Montserrat] text-base">
+              <h6 className="text-[#303030] font-bold font-['Montserrat'] text-base leading-[24px]">
                 Transparent
               </h6>
-              <p className="text-[#303030] font-normal font-[Montserrat] text-base">
+              <p className="text-[#303030] font-normal font-['Montserrat'] text-base">
                 Hassle-free return policy
               </p>
             </div>
